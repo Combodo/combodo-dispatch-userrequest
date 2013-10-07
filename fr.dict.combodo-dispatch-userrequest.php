53cfd1d13f863625f0c3604e85d44a8cb3a3f060
Erwan Taloc <erwan.taloc@combodo.com>
2013-10-07 09:17:44 +0000

    Initial version of Combodo dispatch user request
    SVN:combodo-dispatch-userrequest[2929]

---
+++ fr.dict.combodo-dispatch-userrequest.php
@@ -0,0 +1,7 @@
+<?php
+Dict::Add('FR FR', 'French', 'Français', array(
+	// Dictionary entries go here
+	'Class:UserRequest/Attribute:status/Value:dispatched' => 'Affecté',
+	'Class:UserRequest/Attribute:status/Value:redispatched' => 'Réaffecté',
+	'Class:UserRequest/Stimulus:ev_dispatch' => 'Affecter à une équipe',
+));
