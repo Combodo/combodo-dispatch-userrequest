4fa66854200ed93920da10487a40af9ed9640176
Erwan Taloc <erwan.taloc@combodo.com>
2013-10-07 09:41:47 +0000

    Fix typo in French dictionary
    SVN:trunk[2933]

--- fr.dict.combodo-dispatch-userrequest.php
+++ fr.dict.combodo-dispatch-userrequest.php
@@ -1,7 +1,7 @@
 <?php
 Dict::Add('FR FR', 'French', 'Français', array(
 	// Dictionary entries go here
-	'Class:UserRequest/Attribute:status/Value:dispatched' => 'Affecté',
-	'Class:UserRequest/Attribute:status/Value:redispatched' => 'Réaffecté',
+	'Class:UserRequest/Attribute:status/Value:dispatched' => 'Affectée',
+	'Class:UserRequest/Attribute:status/Value:redispatched' => 'Réaffectée',
 	'Class:UserRequest/Stimulus:ev_dispatch' => 'Affecter à une équipe',
 ));
