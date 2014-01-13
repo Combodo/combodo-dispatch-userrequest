b319ef0b7aec9e57762502f0eb9276c55f18383c
Denis Flaven <denis.flaven@combodo.com>
2014-01-13 12:39:37 +0000

    Module finalization for the Appliance: - Icon and Higlight class take into account the actual SAL computation (no longer assuming 24x7) - New menu "Incident Dispatched to (one of) my Teams" - "flags" state cleanup: it was 100% redondant with the initial definition
    SVN:combodo-dispatch-userrequest[3157]

--- trunk/fr.dict.combodo-dispatch-userrequest.php
+++ trunk/fr.dict.combodo-dispatch-userrequest.php
@@ -4,4 +4,7 @@ Dict::Add('FR FR', 'French', 'Français', array(
 	'Class:UserRequest/Attribute:status/Value:dispatched' => 'Affectée',
 	'Class:UserRequest/Attribute:status/Value:redispatched' => 'Réaffectée',
 	'Class:UserRequest/Stimulus:ev_dispatch' => 'Affecter à une équipe',
+	// Menu entry
+	'Menu:UserRequest:RequestsDispatchedToMyTeams' => 'Requêtes Affectées à mes Equipes',
+	'Menu:UserRequest:RequestsDispatchedToMyTeams+' => 'Requêtes Affectées à une de mes Equipes',
 ));
