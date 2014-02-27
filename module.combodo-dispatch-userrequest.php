e0637e63a5ea98555cd1a4ff3960bf71092724fb
Denis Flaven <denis.flaven@combodo.com>
2014-02-27 17:39:28 +0000

    Adding a version number (1.0.0) to the module
    SVN:trunk[3378]

--- module.combodo-dispatch-userrequest.php
+++ module.combodo-dispatch-userrequest.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-dispatch-userrequest/',
+	'combodo-dispatch-userrequest/1.0.0',
 	array(
 		// Identification
 		//
