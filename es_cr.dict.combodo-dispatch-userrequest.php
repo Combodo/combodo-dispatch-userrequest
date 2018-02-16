d0d60e9143ba00a9c8d839a28d6b41703a53d539
Guillaume Lajarige <guillaume.lajarige@combodo.com>
2018-02-16 10:15:38 +0000

    combodo-dispatch-userrequest: Update spanish translations (Thanks to Miguel Turrubiates!)
    SVN:trunk[6498]

---
+++ es_cr.dict.combodo-dispatch-userrequest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 Combodo SARL
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('ES CR', 'Spanish', 'Español, Castellano', array(
+	// Dictionary entries go here
+	'Class:UserRequest/Attribute:status/Value:dispatched' => 'Despachado',
+	'Class:UserRequest/Attribute:status/Value:redispatched' => 'Redespachado',
+	'Class:UserRequest/Stimulus:ev_dispatch' => 'Despachar a un equipo',
+	// Menu entry
+	'Menu:UserRequest:RequestsDispatchedToMyTeams' => 'Requerimientos despachados a mis equipos',
+	'Menu:UserRequest:RequestsDispatchedToMyTeams+' => 'Requerimientos abiertos despachados a uno de mis equipos',
+));
