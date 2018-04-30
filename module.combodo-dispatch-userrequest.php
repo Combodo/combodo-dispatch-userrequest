2343344e51e51528807765564efefdee9ffddab6
Stephen Abello <stephen.abello@combodo.com>
2018-04-30 13:27:58 +0000

    combodo-dispatch-userrequest: updated german translation and created tag for v1.1.5 release.
    SVN:trunk[6602]

--- module.combodo-dispatch-userrequest.php
+++ module.combodo-dispatch-userrequest.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-dispatch-userrequest/1.1.4',
+	'combodo-dispatch-userrequest/1.1.5',
 	array(
 		// Identification
 		//
