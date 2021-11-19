67ad6aab92b172101761e4e86b66dbe79b233de7
Pierre Goiffon <pierre.goiffon@combodo.com>
2021-11-19 18:40:03 +0100

    :globe_with_meridians: N°3644 Update ES translations Many thanks to Miguel Turrubiates !

--- es_cr.dict.combodo-dispatch-userrequest.php
+++ es_cr.dict.combodo-dispatch-userrequest.php
@@ -1,16 +1,17 @@
 <?php
 /**
- * Localized data
+ * Spanish Localized data
  *
- * @copyright   Copyright (C) 2013 Combodo SARL
+ * @copyright   Copyright (C) 2010-2021 Combodo SARL
  * @license     http://opensource.org/licenses/AGPL-3.0
+ * @traductor   Miguel Turrubiates <miguel_tf@yahoo.com> 
  */
-Dict::Add('ES CR', 'Spanish', 'Español, Castellaño', array(
+Dict::Add('ES CR', 'Spanish', 'Español, Castellano', array(
 	// Dictionary entries go here
 	'Class:UserRequest/Attribute:status/Value:dispatched' => 'Despachado',
 	'Class:UserRequest/Attribute:status/Value:redispatched' => 'Redespachado',
 	'Class:UserRequest/Stimulus:ev_dispatch' => 'Despachar a un equipo',
 	// Menu entry
-	'Menu:UserRequest:RequestsDispatchedToMyTeams' => 'Requerimientos despachados a mis equipos',
-	'Menu:UserRequest:RequestsDispatchedToMyTeams+' => 'Requerimientos despachados a mis equipos de soporte',
+	'Menu:UserRequest:RequestsDispatchedToMyTeams' => 'Requerimientos Despachados a mis Equipos',
+	'Menu:UserRequest:RequestsDispatchedToMyTeams+' => 'Requerimientos Despachados a mis Equipos de Soporte',
 ));
