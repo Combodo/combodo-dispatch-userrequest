7821509afee2e9d8893a7da2164c1324e271720d
Pavel Stetina <63617235+Stetinac@users.noreply.github.com>
2023-06-30 17:51:43 +0200

    🌐 Initial translation cs.dict.combodo-dispatch-userrequest.php

--- cs.dict.combodo-dispatch-userrequest.php
+++ cs.dict.combodo-dispatch-userrequest.php
@@ -22,10 +22,10 @@
  */
 Dict::Add('CS CZ', 'Czech', 'Čeština', array(
 	// Dictionary entries go here
-	'Class:UserRequest/Attribute:status/Value:dispatched' => 'Dispatched~~',
-	'Class:UserRequest/Attribute:status/Value:redispatched' => 'Redispatched~~',
-	'Class:UserRequest/Stimulus:ev_dispatch' => 'Dispatch to a team~~',
+	'Class:UserRequest/Attribute:status/Value:dispatched' => 'Předáno',
+	'Class:UserRequest/Attribute:status/Value:redispatched' => 'Znovu předáno',
+	'Class:UserRequest/Stimulus:ev_dispatch' => 'Předat týmu',
 	// Menu entry
-	'Menu:UserRequest:RequestsDispatchedToMyTeams' => 'Requests dispatched to my teams~~',
-	'Menu:UserRequest:RequestsDispatchedToMyTeams+' => 'Open requests dispatched to one of my teams~~',
+	'Menu:UserRequest:RequestsDispatchedToMyTeams' => 'Požadavky předané mému týmu',
+	'Menu:UserRequest:RequestsDispatchedToMyTeams+' => 'Otevřené požadavky předáné v rámci jednoho z mých týmů',	
 ));
